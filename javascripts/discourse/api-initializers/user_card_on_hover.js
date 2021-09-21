1ec71d83586de51a71573d12b84762c16e327b63
Bianca Nenciu <nenciu.bianca@gmail.com>
2021-09-21 12:51:57 +0300

    DEV: Use card:show and card:hide app events

--- javascripts/discourse/api-initializers/user_card_on_hover.js
+++ javascripts/discourse/api-initializers/user_card_on_hover.js
@@ -5,13 +5,31 @@ export default apiInitializer("0.11.1", (api) => {
     didInsertElement() {
       this._super(...arguments);
 
-      document
-        .querySelector("#main-outlet")
-        .addEventListener("mouseover", this.showCard.bind(this));
+      this.appEvents.on("card:show", this, this.onShow);
+      this.appEvents.on("card:hide", this, this.onHide);
 
-      document
-        .querySelector("#main-outlet")
-        .addEventListener("mouseout", this.hideCard.bind(this));
+      const mainOutlet = document.querySelector("#main-outlet");
+      mainOutlet.addEventListener("mouseover", this.showCard.bind(this));
+      mainOutlet.addEventListener("mouseout", this.hideCard.bind(this));
+    },
+
+    willDestroyElement() {
+      this._super(...arguments);
+
+      this.appEvents.off("card:show", this, this.onShow);
+      this.appEvents.off("card:hide", this, this.onHide);
+
+      const mainOutlet = document.querySelector("#main-outlet");
+      mainOutlet.removeEventListener("mouseover", this.showCard.bind(this));
+      mainOutlet.removeEventListener("mouseout", this.hideCard.bind(this));
+    },
+
+    onShow(_username, _target, event) {
+      this.set("lastEvent", event);
+    },
+
+    onHide() {
+      this.set("lastEvent", null);
     },
 
     showCard(event) {
